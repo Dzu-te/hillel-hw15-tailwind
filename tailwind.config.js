9aadeaf46ce204d5d32d2195c9c937bb0e9ebb25
Yurii Pochapskiy <pochapskiy1983@gmail.com>
2025-01-05 20:11:29 +0200

    mobile&tablet

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    screens: {
+
+      'tb': '640px',
+      'ds': '1280px',
+    },
+    extend: {
+      backgroundImage: {
+        'burger-header-menu': "url('../svg/header-burger.svg')",
+        'burger-button': "url('../svg/catalog-burger.svg')",
+        'hero-main': "url('../pictures/bgi-main.png')",
+        'hero-main-tb': "url('../pictures/bgi-main-tb.png')",
+      },
+      colors: {
+        'custom-blue': '#01B7E6',
+        'color-two': '#587478',
+        'footer-color': '#777777',
+
+      },
+    },
+  },
+  plugins: [],
+}
+
